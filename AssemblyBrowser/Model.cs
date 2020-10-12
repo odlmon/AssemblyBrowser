9c93f15625eb5fab56410de60f27e353fcdd9ed1
odlmon <dmi3yosipov@gmail.com>
2020-10-12 16:57:08 +0300

    Add: Model and ViewModel with OpenCommand for MVVM

---
+++ AssemblyBrowser/Model.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using AssemblyReader;
+
+namespace AssemblyBrowser
+{
+    public class Model : INotifyPropertyChanged
+    {
+        private string _assemblyFile;
+        public string AssemblyFile
+        {
+            get => _assemblyFile;
+            set
+            {
+                _assemblyFile = value;
+                OnPropertyChanged("AssemblyFile");
+            }
+        }
+
+        private ObservableCollection<AssemblyInfo> _assemblyInfo;
+        public ObservableCollection<AssemblyInfo> AssemblyInfo
+        {
+            get => _assemblyInfo;
+            set
+            {
+                _assemblyInfo = value;
+                OnPropertyChanged("AssemblyInfo");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ AssemblyBrowser/ViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using AssemblyReader;
+using Microsoft.Win32;
+
+namespace AssemblyBrowser
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private Model _model;
+
+        public ViewModel(Model model)
+        {
+            _model = model;
+        }
+
+        public string AssemblyFile
+        {
+            get => _model.AssemblyFile;
+            set
+            {
+                _model.AssemblyFile = value;
+                this.AssemblyInfo = new ObservableCollection<AssemblyInfo>
+                {
+                    AssemblyReader.AssemblyReader.GetAssemblyInfo(_model.AssemblyFile)
+                };
+                OnPropertyChanged("AssemblyFile");
+            }
+        }
+
+        public ObservableCollection<AssemblyInfo> AssemblyInfo
+        {
+            get => _model.AssemblyInfo;
+            set
+            {
+                _model.AssemblyInfo = value;
+                OnPropertyChanged("AssemblyInfo");
+            }
+        }
+
+        private ICommand _openCommand;
+        public ICommand OpenCommand
+        {
+            get
+            {
+                return _openCommand ??= new RelayCommand(obj =>
+                {
+                    OpenFileDialog openFileDialog = new OpenFileDialog();
+                    if (openFileDialog.ShowDialog() == true)
+                    {
+                        AssemblyFile = openFileDialog.FileName;
+                    }
+                });
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
